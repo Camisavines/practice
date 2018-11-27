f7947c73eeafb6c48c30c9f54d43fdd8ed2cee43
Camisa Vines <camisa.vines@icloud.com>
2018-11-26 23:01:59 -0500

    styling

--- Dividers/main.js
+++ Dividers/main.js
@@ -4,8 +4,11 @@
 
   var repeat = "<section><h2>filler section</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></section>"
 
+  var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
   function init() {
     $(".repeat").append(repeat);
+    $(".lorem").append(lorem);
   }
 
 })();
